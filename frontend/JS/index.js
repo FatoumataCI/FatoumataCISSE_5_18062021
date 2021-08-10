2a30e0a9b74cf1240bab71ae4d6b91a887439b56
CISSE Fatoumata <fatou.ycisse@gmail.com>
2021-08-10 13:28:20 +0200

    index.js correction

--- frontend/JS/index.js
+++ frontend/JS/index.js
@@ -18,14 +18,14 @@ function getData() {
 
             let div =
                 `<div class="col">
-                    <div class="card">
+                    <div class="card">                    
                         <img src="${imageUrl}" class="card-img-top"  alt="..." width="100" height="200">
                             <div class="card-body">
                                 <h5 class="card-title"> ${name} </h5>
                                 <p class="card-description"> ${description}</p>
                                 <p class="card-text"> ${price}€</p>
-                                <a href="produit.html?id=${id}" class="btn btn-primary">Voir l'article</a>
-                            </div>
+                                <a href="produit.html?id=${id}" class="btn btn-primary"> Voir l'article </a>
+                            </div>                           
                     </div>
                 </div>` 
                     madiv.innerHTML += div;  
